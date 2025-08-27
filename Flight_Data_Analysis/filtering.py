0a25c6117cdfa14162c2f27cb331c9c4ef64afc0
Daniel <daniel.w.garcia@gmail.com>
2025-08-27 11:27:40 -0700

    pandas filtering and grouping

---
+++ Flight_Data_Analysis/filtering.py
@@ -0,0 +1,39 @@
+﻿#%%
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+#%%
+df = pd.read_csv('flights.csv')
+
+print(df.head(10))
+print(df.info())
+print(df.isnull().sum())
+print(df.describe().round(2))
+print(df['Origin'].value_counts())
+#%%
+df['Year'] = df['Fly Date'].astype(str).str.slice(0,4)
+
+print(df['Year'])
+flights_in_2000 = df[df['Year'] == '2000']
+print(flights_in_2000.head())
+#%%
+non_lax_flights = df[~df['Origin'].isin(['LAX'])] #tilde negates. so really saying 'is not in'
+print(non_lax_flights.head())
+print('LAX' in non_lax_flights['Origin'].unique())
+#%%
+passengers_and_distance = df[(df['Passengers'] > 100) & (df['Distance'] < 1000)]
+print(passengers_and_distance[['Passengers', 'Distance']])
+#%%
+ny_la = df[(df['Origin City']).str.contains('New York|Los Angeles') | (df['Destination City'].str.contains('New York|Los Angeles'))]
+print(ny_la[['Origin City', 'Destination City']])
+print(ny_la.info())
+#%%
+large_origin = df[df['Origin Population'] > 1000000]
+print(large_origin[['Origin Population', 'Origin City']])
+#%%
+underbooked = df.query('Seats > Passengers')
+print(underbooked[['Seats', 'Passengers']])
+#%%
+busy_shorthaul = df.query('Distance < 500 & Flights > 10')
+print(busy_shorthaul [['Distance', 'Flights']])
+busy_shorthaul.to_csv('shorthaul.csv')
---
+++ Flight_Data_Analysis/grouping_and_aggregating.py
@@ -0,0 +1,37 @@
+﻿import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+#%%
+df = pd.read_csv('flights.csv')
+
+#%%
+busiest_routes = df.groupby(['Origin', 'Destination']).agg({'Passengers' : 'sum'})
+busiest_routes = busiest_routes.sort_values(by='Passengers', ascending=False)
+print(busiest_routes.head())
+
+#%%
+busiest_routes = df.groupby(['Origin', 'Destination']).agg({'Passengers' : 'sum', 'Flights' : 'sum'})
+busiest_routes = busiest_routes.sort_values(by=['Passengers', 'Flights'], ascending=[False, False])
+print(busiest_routes.head(10))
+
+#%%
+df['Month'] = pd.to_datetime(df['Fly Date'], format='%Y%m').dt.to_period('M')
+monthly_trends = df.groupby('Month').agg({'Flights': 'sum'})
+monthly_trends = monthly_trends.sort_values(by='Month')
+print(monthly_trends)
+
+#%%
+distance_bins = pd.cut(df['Distance'], bins=[0, 500, 1000, 2000, 3000, 4000, df['Distance'].max()])
+distance_group = df.groupby(distance_bins).agg({'Flights': 'sum', 'Passengers': 'sum'})
+print(distance_group.sort_values(by='Distance', ascending=False))
+
+#%%
+busiest_airports = df.groupby('Origin').agg({'Flights': 'sum'}).nlargest(10, 'Flights')
+busiest_airports.plot(kind='bar')
+plt.title('Top 10 Busiest Airports by Outgoing Flights')
+plt.xlabel('Airport')
+plt.ylabel('Number of Flights')
+plt.xticks(rotation=45)
+plt.tight_layout()
+plt.show()
